66a628e119faadb2d0f52884dca2f74769b57fd2
Tio Saputra <karya.tiosaputra@gmail.com>
2020-05-07 00:27:42 +0700

    add book and add author

--- server.js
+++ server.js
@@ -92,8 +92,52 @@ const RootQueryType = new GraphQLObjectType({
   }),
 });
 
+const RootMutationType = new GraphQLObjectType({
+  name: "Mutation",
+  description: "root mutation",
+  fields: () => ({
+    addBook: {
+      type: BookType,
+      description: "add book",
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+        authorId: { type: GraphQLNonNull(GraphQLInt) },
+      },
+      resolve: (parent, args) => {
+        const book = {
+          id: books.length + 1,
+          name: args.name,
+          authorId: args.authorId,
+        };
+
+        books.push(book);
+
+        return book;
+      },
+    },
+    addAuthor: {
+      type: AuthorType,
+      description: "add an author",
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve: (parent, args) => {
+        const author = {
+          id: authors.length + 1,
+          name: args.name,
+        };
+
+        authors.push(author);
+
+        return author;
+      },
+    },
+  }),
+});
+
 const schema = new GraphQLSchema({
   query: RootQueryType,
+  mutation: RootMutationType,
 });
 
 app.use(
